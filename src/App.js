da579535ad70ad51f8b0dfa6572b893716881d54
Behruzbek2023 <123858278+Behruzbek2023@users.noreply.github.com>
2023-01-29 17:46:43 +0300

    Redux_Darslari

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {Routes,Route} from "react-router-dom"
+import Main from "./conponets/Main"
+import Login from "./conponets/Login"
+import Register from "./conponets/Register"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Routes>
+       <Route path="/" element={<Main/>}/>
+       <Route path="/Login" element={<Login/>}/>
+       <Route path="/Register" element={<Register/>}/>
+     </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
